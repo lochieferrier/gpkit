33da088622c4e4facca54384e16aa19a70bf42a5
Warren Hoburg <whoburg@mit.edu>
2016-03-12 17:10:13 -0500

    abstract base classing in progress

--- gpkit/constraints/single_equation.py
+++ gpkit/constraints/single_equation.py
@@ -1,18 +1,34 @@
 "Implements SingleEquationConstraint"
+from abc import ABCMeta, abstractproperty, abstractmethod
 from operator import le, ge, eq
 from ..small_scripts import try_str_without
 from ..repr_conventions import _str, _repr, _repr_latex_
 
 
-class SingleEquationConstraint(object):
+class SingleEquationConstraint:
     "Constraint expressible in a single equation."
     latex_opers = {"<=": "\\leq", ">=": "\\geq", "=": "="}
     func_opers = {"<=": le, ">=": ge, "=": eq}
+
+    __metaclass__ = ABCMeta
 
     __str__ = _str
     __repr__ = _repr
     _repr_latex_ = _repr_latex_
 
+    @abstractproperty
+    def left(self):
+        pass
+
+    @abstractproperty
+    def right(self):
+        pass
+
+    @abstractproperty
+    def oper(self):
+        pass
+
+    @abstractmethod
     def str_without(self, excluded=None):
         if excluded is None:
             excluded = ["units"]
@@ -20,14 +36,17 @@ class SingleEquationConstraint(object):
                              self.oper,
                              try_str_without(self.right, excluded))
 
+    @abstractmethod
     def subconstr_str(self, excluded):
         "The collapsed string of a constraint"
         pass
 
+    @abstractmethod
     def subconstr_latex(self, excluded):
         "The collapsed latex of a constraint"
         pass
 
+    @abstractmethod
     def latex(self, excluded=None):
         if not excluded:
             excluded = ["units"]  # previously bool(self.left.units)
